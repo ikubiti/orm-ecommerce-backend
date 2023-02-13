0811e24d10f5f68eb839df1950f1c44a6a500fee
Isoyor Kubiti <ikubiti@icloud.com>
2023-02-13 11:31:43 -0500

    setup sequelize to connect the database and start app server

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      port: process.env.DB_PORT,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
 
 module.exports = sequelize;
